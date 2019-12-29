5d7e2002e9b325e913a1dbb156f151928d498996
QuinceyNiu <490332924@qq.com>
2019-12-29 17:30:55 +0800

    Update marathon.cpp

--- marathon.cpp
+++ marathon.cpp
@@ -32,6 +32,8 @@
 对于 30% 的数据，有 1 ≤ n ≤ 50。
 对于 100% 的数据，有 1 ≤ n≤ 5,000。
 */
+
+/*attempt1
 #include <iostream>
 #define N 15000
 #define M 5000
@@ -76,4 +78,37 @@ int main() {
     }
 
     return 0;
+}*/
+
+//attempt2
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <deque>
+using namespace std;
+struct node
+{
+    int data;
+    int data2;
+    int data3;
+    bool operator < (node nodeNew)
+    {
+        return data*10000+data2*100+data3<nodeNew.data*10000+nodeNew.data2*100+nodeNew.data3 ? 1:0;
+    }
+};
+int main()
+{
+    unsigned int number;
+    scanf("%d",&number);
+    deque <node> myList;
+    for(int i=0;i<number;i++)
+    {
+        node time;
+        scanf("%d %d %d",&time.data,&time.data2,&time.data3);
+        myList.push_back(time);
+    }
+    deque<node>::iterator i;
+    sort(myList.begin(),myList.end());
+    for(i=myList.begin();i!=myList.end();i++)
+        cout<<(*i).data<<" "<<(*i).data2<<" "<<(*i).data3<<endl;
 }
